4866615f681222bff8471b211dd667672bb3a29a
Jarosław Bagnicki <jarek@bagnicki.net>
2020-01-10 14:18:03 +0100

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true,
+        "mocha": true,
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-unused-vars": "warn",
+        "semi": "error",
+        "quotes": ["error", "single"],
+        "comma-dangle": ["warn", "always-multiline"],
+    }
+  };
+  
